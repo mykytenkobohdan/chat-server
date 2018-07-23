8d8db12e51f97e23632cd4a2cc42ea1e340e006b
bohdan <myketenko.bohdan@gmail.com>
2018-07-23 17:51:51 +0300

    Compared password.

--- routes/authorize.js
+++ routes/authorize.js
@@ -5,8 +5,11 @@ var User = require('../models/user');
 /* POST check authorize. */
 router.post('/', function (req, res, next) {
     User.find({'username': req.body.username}, function (err, user) {
-        console.log(user);
-        res.json(user);
+        // Add compare password with hash;
+        if (user.length > 0 && user[0].password === req.body.password) {
+            return res.json(user);
+        }
+        return res.json([]);
     });
 });
 
