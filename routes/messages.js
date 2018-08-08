8915c374914f9f9beb3e4ceea595f27938cbb46a
mykytenkobohdan <mykytenko.bohdan@gmail.com>
2018-08-08 20:18:58 +0300

    Added sort by date craeted.

--- routes/messages.js
+++ routes/messages.js
@@ -4,7 +4,16 @@ var router = express.Router();
 
 /* GET get all messages. */
 router.get('/', function (req, res, next) {
-    Message.find({}, function (err, messages) {
+    const query = {}
+    const projection = {}
+    // const options = { sort: { id: 1 }, limit: 2, skip: 10 }
+    const options = {
+        sort: {
+            createdAt: 1
+        }
+    }
+
+    Message.find(query, projection, options).exec(function (err, messages) {
         messages.forEach(function (item) {
             if (item.isRemoved) {;
                 // delete item['message'];
@@ -19,11 +28,17 @@ router.get('/', function (req, res, next) {
 /* POST create message. */
 router.post('/', function (req, res, next) {
     Message.create(req.body, function (err, message) {
-        if (err) return res.json({status: 500, data: err});
+        if (err) return res.json({
+            status: 500,
+            data: err
+        });
 
         var io = req.app.get('socketio');
         io.emit('message', message);
-        return res.json({status: 200, message: 'Message created!'});
+        return res.json({
+            status: 200,
+            message: 'Message created!'
+        });
     });
 });
 
@@ -38,7 +53,11 @@ router.put('/', function (req, res, next) {
 
             io.emit('update-message', message);
             if (err) return handleError(err);
-            res.json({status: 200, message: 'Message updated!', data: updatedMessage});
+            res.json({
+                status: 200,
+                message: 'Message updated!',
+                data: updatedMessage
+            });
         });
     });
 });
@@ -48,12 +67,15 @@ router.delete('/', function (req, res, next) {
     Message.findById(req.query.id, function (err, message) {
         message.isRemoved = true;
         message.save(function (err, ms) {
-            delete ms.message;
+            ms.message = undefined;
 
             var io = req.app.get('socketio');
             io.emit('remove-message', ms);
             if (err) return handleError(err);
-            res.json({status: 200, message: 'Message removed!'});
+            res.json({
+                status: 200,
+                message: 'Message removed!'
+            });
         });
     });
 });
