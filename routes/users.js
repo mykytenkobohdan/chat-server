b6e92a9e00c4b5c4e4e077caac78287f34fc235f
bohdan <myketenko.bohdan@gmail.com>
2018-08-10 13:34:11 +0300

    Fixed user response

--- routes/users.js
+++ routes/users.js
@@ -4,39 +4,43 @@ var router = express.Router();
 
 /* GET get all users. */
 router.get('/', function (req, res, next) {
-    User.find({}, function (err, users) {
-        res.json(users);
-    });
+  User.find({}, function (err, users) {
+    res.json(users);
+  });
 });
 
 /* GET get current user. */
 router.get('/:id', function (req, res, next) {
-    console.log('Get user by ID: ', req.params);
-
-    User.find({'_id': req.params.id}, function (err, user) {
-        res.json(user);
-    });
+  User.findById(req.params.id, function (err, user) {
+    res.json(user);
+  });
 });
 
 /* POST create user. */
 router.post('/', function (req, res, next) {
-    User.create(req.body, function (err, user) {
-        if (err) {
-            console.log(err);
-            return res.json({error: true, errorMessage: err.message})
-        }
+  User.create(req.body, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.json({
+        error: true,
+        errorMessage: err.message
+      })
+    }
 
-        return res.json({status: 200, message: 'User created!'});
+    return res.json({
+      status: 200,
+      message: 'User created!'
     });
+  });
 });
 
 /* PUT update user. */
 router.put('/', function (req, res, next) {
-    console.log('PUT update user: ', req.body);
+  console.log('PUT update user: ', req.body);
 
-    User.findByIdAndUpdate(req.body, function (err, user) {
-        res.json(user);
-    });
+  User.findByIdAndUpdate(req.body, function (err, user) {
+    res.json(user);
+  });
 });
 
 module.exports = router;
